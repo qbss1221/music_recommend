c55bc337a8f6dd93d094feed75945ac7d7fab66c
胡茜茜 <huxixi@limefamily.com>
2018-04-01 16:08:15 +0800

    见上一次提交信息。

--- apps/users/adminx.py
+++ apps/users/adminx.py
@@ -1,3 +1,14 @@
-# _*_ coding: utf-8  _*_
+# -*- coding: utf-8 -*-
 __author__ = 'qbss'
 __date__ = '2018/4/1 14:29'
+
+import xadmin
+
+from .models import EmailVerifyRecord
+
+
+class EmailVerifyRecordAdmin(object):
+    list_display = ['code']
+
+
+xadmin.site.register(EmailVerifyRecord,EmailVerifyRecordAdmin)
