4d48e4ec0539575cfae64f36320cf7aa3b0e91e4
胡茜茜 <huxixi@limefamily.com>
2018-04-01 17:18:23 +0800

    将后台数据表的列表展示、搜索、过滤器所需字段加上。 邮箱验证、轮播图两个表。

--- apps/users/adminx.py
+++ apps/users/adminx.py
@@ -1,14 +1,23 @@
 # -*- coding: utf-8 -*-
-__author__ = 'qbss'
+__author__ = 'huxixi'
 __date__ = '2018/4/1 14:29'
 
 import xadmin
 
-from .models import EmailVerifyRecord
+from .models import EmailVerifyRecord, Banner
 
 
 class EmailVerifyRecordAdmin(object):
-    list_display = ['code']
+    list_display = ['code', 'email', 'send_type', 'send_time']  # 列表展示
+    search_fields = ['code', 'email', 'send_type']  # 搜索字段
+    list_filter = ['code', 'email', 'send_type', 'send_time']  # 过滤器
+
+
+class BannerAdmin(object):
+    list_display = ['title', 'image', 'url', 'index', 'add_time']
+    search_fields = ['title', 'image', 'url', 'index']
+    list_filter = ['title', 'image', 'url', 'index', 'add_time']
 
 
 xadmin.site.register(EmailVerifyRecord,EmailVerifyRecordAdmin)
+xadmin.site.register(Banner, BannerAdmin)
