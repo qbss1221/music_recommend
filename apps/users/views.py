4b533955d68ecc29671757f7f9cb9b6447302897
胡茜茜 <huxixi@limefamily.com>
2018-04-09 19:58:19 +0800

    与上一次提交信息相同

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login
 
-# Create your views here.
+
+# 6.2  13:18
+def login1(request):
+    if request.method == "POST":
+        user_name = request.POST.get("username", "")
+        pass_word = request.POST.get("password", "")
+        user = authenticate(username=user_name, password=pass_word)
+        if user is not None:
+            login(request, user)
+            return render(request, "index.html")
+        else:
+            return render(request, "login.html", {})
+    elif request.method == "GET":
+        return render(request, "login.html", {})
+
--- music/urls.py
+++ music/urls.py
@@ -18,8 +18,10 @@ from django.urls import path
 from django.views.generic import TemplateView
 import xadmin
 
+from users.views import login1
+
 urlpatterns = [
     path('xadmin/', xadmin.site.urls),
     path('', TemplateView.as_view(template_name="index.html"), name="index"),
-    path('login/', TemplateView.as_view(template_name="login.html"), name="login"),
+    path('login/', login1),
 ]
