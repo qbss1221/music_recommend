fc11642141244f3934f77a574a1521ca9905e2ac
胡茜茜 <huxixi@limefamily.com>
2018-04-10 19:21:55 +0800

    写customBaclend实现输入邮箱登录, 加入错误提示

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,9 +1,24 @@
+# _*_ coding:utf-8 _*_
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.backends import ModelBackend
+from django.db.models import Q  # Q表示或
+
+from .models import UserProfile
 
 
-# 6.2  13:18
-def login1(request):
+# 实现邮箱登陆
+class CustomBackend(ModelBackend):  # 通过邮箱登陆
+    def authenticate(self, username=None, password=None, **kwargs):
+        try:
+            user = UserProfile.objects.get(Q(username=username)|Q(email=username))
+            if user.check_password(password):
+                return user
+        except Exception as e:
+            return None
+
+
+def user_login(request):
     if request.method == "POST":
         user_name = request.POST.get("username", "")
         pass_word = request.POST.get("password", "")
@@ -12,7 +27,7 @@ def login1(request):
             login(request, user)
             return render(request, "index.html")
         else:
-            return render(request, "login.html", {})
+            return render(request, "login.html", {"msg": "用户名或密码错误！"})
     elif request.method == "GET":
         return render(request, "login.html", {})
 
--- music/urls.py
+++ music/urls.py
@@ -18,10 +18,10 @@ from django.urls import path
 from django.views.generic import TemplateView
 import xadmin
 
-from users.views import login1
+from users.views import user_login
 
 urlpatterns = [
     path('xadmin/', xadmin.site.urls),
     path('', TemplateView.as_view(template_name="index.html"), name="index"),
-    path('login/', login1),
+    path('login/', user_login),
 ]
